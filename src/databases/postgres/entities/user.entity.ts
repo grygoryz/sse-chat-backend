a14c986b8c23e4f1fd3fca81004d06080e9581a7
Grigory Tsyganok <tsyganok.g@future-group.ru>
2022-01-08 16:26:04 +0300

    create user entity

---
+++ src/databases/postgres/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column('varchar', { length: 50 })
+	name!: string;
+}
