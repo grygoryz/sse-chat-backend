efbe5d64c3c0dc94a2114ea47a6e8a2829682025
Grigory Tsyganok <tsyganok.g@future-group.ru>
2022-01-14 18:42:57 +0300

    user-connected-to-chat guard unit test

---
+++ src/chat/guards/user-connected-to-chat.guard.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserConnectedToChatGuard } from './user-connected-to-chat.guard';
+import { ChatRedisRepository } from '../chat-redis.repository';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { ExecutionContext } from '@nestjs/common';
+
+describe('UserConnectedToChatGuard', () => {
+	let guard: UserConnectedToChatGuard;
+	let chatRedisRepository: DeepMocked<ChatRedisRepository>;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UserConnectedToChatGuard,
+				{
+					provide: ChatRedisRepository,
+					useValue: createMock<ChatRedisRepository>(),
+				},
+			],
+		}).compile();
+
+		guard = module.get(UserConnectedToChatGuard);
+		chatRedisRepository = module.get(ChatRedisRepository);
+	});
+
+	it('should throw error if user not connected', async () => {
+		const executionContext = createMock<ExecutionContext>();
+		executionContext.switchToHttp().getRequest.mockReturnValue({ session: { user: { id: 1 } } });
+		chatRedisRepository.isUserConnected.mockResolvedValue(false);
+
+		await expect(guard.canActivate(executionContext)).rejects.toThrow();
+	});
+
+	it('should pass if user connected', async () => {
+		const executionContext = createMock<ExecutionContext>();
+		executionContext.switchToHttp().getRequest.mockReturnValue({ session: { user: { id: 1 } } });
+		chatRedisRepository.isUserConnected.mockResolvedValue(true);
+
+		const response = await guard.canActivate(executionContext);
+
+		await expect(response).toBe(true);
+	});
+});
